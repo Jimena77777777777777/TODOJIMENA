4265a7abf1baba33b9102c7d97ec47bd268158f5
Alejandra <202100355d@utea.edu.pe>
2024-08-10 11:48:28 -0500

    nuevo login y register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,47 +21,74 @@
     <body>
 
         <section class="vh-100" style="background-color: #9A616D;">
-            <div class="container py-5 h-100">
-              <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col col-xl-10">
-                  <div class="card" style="border-radius: 1rem;">
-                    <div class="row g-0">
-                      <div class="col-md-6 col-lg-5 d-none d-md-block">
-                        <img src="https://i.pinimg.com/736x/6c/43/02/6c4302f1a5415158d9d21bbc5948db7e.jpg"
-                          alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
-                      </div>
-                      <div class="col-md-6 col-lg-7 d-flex align-items-center">
-                        <div class="card-body p-4 p-lg-5 text-black">
+                    <div class="card" style="border-radius: 1rem;">
+                        <div class="row g-0">
+                            <div class="col-md-6 col-lg-5 d-none d-md-block">
+                                <img src="https://i.pinimg.com/564x/46/57/0a/46570af73bc6fd59a9de7be377871857.jpg"
+                                    alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem; object-fit: cover; height: 100%;" />
+                            </div>
+                            <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                                <div class="card-body p-4 p-lg-5 text-black">
+        
           
                           <form action="{{route("login")}}" method="POST">
                             @csrf
 
           
-                            <div class="d-flex align-items-center mb-3 pb-1">
-                              <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
-                              <span class="h1 fw-bold mb-0">TODOLIST</span>
-                            </div>
+                            <div class="d-flex justify-content-center align-items-center mb-3 pb-1">
+                              <i class="fas fa-cubes fa-2x me-3" style="color: #ff8fab;"></i>
+                              <span class="h1 fw-bold mb-0" style="color: #ff8fab;">TODOLIST</span>
+                          </div>
           
-                            <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Registra cada una de tus tareas pendientes y mantén todo bajo control.</h5>
+                          
+                          
+                          <h5 class="fw-light mb-3 pb-3" style="letter-spacing: 1px; font-family: 'Georgia', serif; font-size: 1.125rem; color: #333; text-align: center;">
+                            Registra cada una de tus tareas pendientes y mantén todo bajo control.
+                        </h5>
+                        
+
+                        <div data-mdb-input-init class="form-outline mb-4">
+                          <input type="email" name="email" id="form2Example17" class="form-control form-control-lg" />
+                          <label class="form-label" for="form2Example17" style="color: #ff8fab;">E-mail 
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-open-fill" viewBox="0 0 16 16">
+                              <path d="M8.941.435a2 2 0 0 0-1.882 0l-6 3.2A2 2 0 0 0 0 5.4v.314l6.709 3.932L8 8.928l1.291.718L16 5.714V5.4a2 2 0 0 0-1.059-1.765zM16 6.873l-5.693 3.337L16 13.372v-6.5Zm-.059 7.611L8 10.072.059 14.484A2 2 0 0 0 2 16h12a2 2 0 0 0 1.941-1.516M0 13.373l5.693-3.163L0 6.873z"/>
+                            </svg>  
+                          </label>
+                        </div>
+                        
           
-                            <div data-mdb-input-init class="form-outline mb-4">
-                              <input type="email" name="email" id="form2Example17" class="form-control form-control-lg" />
-                              <label class="form-label" for="form2Example17">E-mail</label>
-                            </div>
+                            
           
                             <div data-mdb-input-init class="form-outline mb-4">
                               <input type="password" name="password" id="form2Example27" class="form-control form-control-lg" />
-                              <label class="form-label" for="form2Example27">Contraseña</label>
+                              <label class="form-label" for="form2Example27" style="color: #ff8fab;">Contraseña
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-key-fill" viewBox="0 0 16 16">
+                                  <path d="M3.5 11.5a3.5 3.5 0 1 1 3.163-5H14L15.5 8 14 9.5l-1-1-1 1-1-1-1 1-1-1-1 1H6.663a3.5 3.5 0 0 1-3.163 2M2.5 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
+                                </svg>
+                              </label>
                             </div>
           
-                            <div class="pt-1 mb-4">
-                              <button data-mdb-button-init data-mdb-ripple-init class="btn btn-dark btn-lg btn-block" type="submit">Ingresar</button>
-                            </div>
-          
+                            <div class="text-center pt-1 mb-4">
+                              <button data-mdb-button-init data-mdb-ripple-init class="btn btn-block" type="submit"
+                                  style="background-color: #333; color: white; border: none; font-weight: 700; font-size: 1.25rem; padding: 10px 25px; border-radius: 50px;">
+                                  Ingresar
+                              </button>
+                          </div>
                           
-                            <p class="mb-5 pb-lg-2" style="color: #393f81;">¿Aún no tienes una cuenta?  </p>
-
-                                <a href="{{route("register")}}" class="btn btn-dark btn-lg btn-block"">Registrate con nosotros</a>
+                          
+                            <div class="text-center">
+                              <p class="mb-3 pb-lg-2" style="color: #ffafcc; font-weight: 600;">¿Aún no tienes una cuenta?  </p>
+                          </div>
+                          
+                          <div class="d-flex justify-content-center">
+                            <a href="{{route('register')}}" class="btn btn-lg btn-block" style="background-color: #ffafcc; color: white; border: none; max-width: 300px; font-weight: 700; font-size: 1.25rem; padding: 10px 25px; border-radius: 50px;">
+                                Regístrate 
+                            </a>
+                        </div>
+                        
                           
                           </form>
           
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -20,58 +20,87 @@
 
     <body>
 
-        <section class="vh-100" style="background-color: #9A616D;">
-            <div class="container py-5 h-100">
-              <div class="row d-flex justify-content-center align-items-center h-100">
+        <section class="vh-100" style="background-color: #9c89b8;">
+          <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col col-xl-10">
-                  <div class="card" style="border-radius: 1rem;">
-                    <div class="row g-0">
-                      <div class="col-md-6 col-lg-5 d-none d-md-block">
-                        <img src="https://i.pinimg.com/736x/6c/43/02/6c4302f1a5415158d9d21bbc5948db7e.jpg"
-                          alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
-                      </div>
-                      <div class="col-md-6 col-lg-7 d-flex align-items-center">
-                        <div class="card-body p-4 p-lg-5 text-black">
-          
-                         
+                    <div class="card" style="border-radius: 1rem;">
+                        <div class="row g-0">
+                            <div class="col-md-6 col-lg-5 d-none d-md-block">
+                                <img src="https://i.pinimg.com/564x/37/bc/ad/37bcad8ff329598b5698ab443a76d81c.jpg"
+                                    alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem; object-fit: cover; height: 100%;" />
+                            </div>
+                            <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                                <div class="card-body p-4 p-lg-5 text-black">
+        
 
 
                             <form action="{{route('register')}}" method="post">
                                 @csrf
                                 
-                                <p>INICIAR SESION</p>
+                                <div class="d-flex justify-content-center align-items-center mb-3 pb-1">
+                                  <i class="fas fa-cubes fa-2x me-3" style="color: #9c89b8;;"></i>
+                                  <span class="h1 fw-bold mb-0" style="color: #9c89b8;;">INICIAR SESIÓN</span>
+                              </div>
+                              
+                           
     
                                 <div data-mdb-input-init class="form-outline mb-4">
-                                    <label class="form-label" for="form2Example11">Nombre</label>
+                                    <label class="form-label" for="form2Example11">Nombre
+                                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen-fill" viewBox="0 0 16 16">
+                                        <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001"/>
+                                      </svg>
+                                    </label>
                                   <input type="text" name="name" id="form2Example11" class="form-control"
                                     placeholder="Ingresar Nombre" />
                                 </div>
               
                                 <div data-mdb-input-init class="form-outline mb-4">
-                                    <label class="form-label" for="form2Example11">Correo</label>
+                                    <label class="form-label" for="form2Example11">Correo
+                                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-open-fill" viewBox="0 0 16 16">
+                                        <path d="M8.941.435a2 2 0 0 0-1.882 0l-6 3.2A2 2 0 0 0 0 5.4v.314l6.709 3.932L8 8.928l1.291.718L16 5.714V5.4a2 2 0 0 0-1.059-1.765zM16 6.873l-5.693 3.337L16 13.372v-6.5Zm-.059 7.611L8 10.072.059 14.484A2 2 0 0 0 2 16h12a2 2 0 0 0 1.941-1.516M0 13.373l5.693-3.163L0 6.873z"/>
+                                      </svg>  
+                                    </label>
                                   <input type="email" name="email" id="form2Example11" class="form-control"
                                     placeholder="Ingresar correo" />
                                 </div>
               
                                 <div data-mdb-input-init class="form-outline mb-4">
-                                    <label class="form-label" for="form2Example22">Contraseña</label>
+                                    <label class="form-label" for="form2Example22">Contraseña
+                                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-key-fill" viewBox="0 0 16 16">
+                                        <path d="M3.5 11.5a3.5 3.5 0 1 1 3.163-5H14L15.5 8 14 9.5l-1-1-1 1-1-1-1 1-1-1-1 1H6.663a3.5 3.5 0 0 1-3.163 2M2.5 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
+                                      </svg>
+                                    </label>
                                   <input type="password" name="password" id="form2Example22" class="form-control" /> 
                                 </div>
     
                                 
                                 <div data-mdb-input-init class="form-outline mb-4">
-                                    <label class="form-label" for="form2Example22">Confirmar Contraseña</label>
+                                    <label class="form-label" for="form2Example22">Confirmar Contraseña
+                                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-key-fill" viewBox="0 0 16 16">
+                                        <path d="M3.5 11.5a3.5 3.5 0 1 1 3.163-5H14L15.5 8 14 9.5l-1-1-1 1-1-1-1 1-1-1-1 1H6.663a3.5 3.5 0 0 1-3.163 2M2.5 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
+                                      </svg>
+                                    </label>
                                   <input type="password" name="password_confirmation" id="form2Example22" class="form-control" /> 
                                 </div>
               
-                                <div class="text-center pt-1 mb-5 pb-1">
-                                  <button data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="submit">Registrarse</button>
-                                </div>
+                                <div class="text-center pt-1 mb-3 pb-1">
+                                  <button data-mdb-button-init data-mdb-ripple-init class="btn btn-block fa-lg mb-3" type="submit" 
+                                      style="background-color: #e7c6ff; color: white; border: none; font-weight: 700; font-size: 1.25rem; padding: 10px 25px; border-radius: 50px;">
+                                      Registrarse
+                                  </button>
+                              </div>
+                              
+                                
               
-                                <div class="d-flex align-items-center justify-content-center pb-4">
-                                  <p class="mb-0 me-2">Regresar a Inicio?</p>
-                                  <a href="{{route('login')}}" data-mdb-button-init data-mdb-ripple-init class="btn btn-outline-danger">Regresar</a>
-                                </div>
+                              <div class="d-flex align-items-center justify-content-center pb-0">
+                                <p class="mb-0 me-2">Regresar a Inicio?</p>
+                                <a href="{{route('login')}}" data-mdb-button-init data-mdb-ripple-init class="btn btn-outline-dark" 
+                                    style="color: #9c89b8; border-color: #9c89b8; border-radius: 50px; padding: 10px 25px; font-weight: 700; font-size: 1rem;">
+                                    Regresar
+                                </a>
+                            </div>
+                            
               
                               </form>
           
